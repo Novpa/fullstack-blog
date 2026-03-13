b997d405faaf578dbf1e87578c46d8a658e9ef1e
Novpa <aa.novparadiputra@binus.ac.id>
2026-03-13 16:14:53 +0700

    refactor: remove isOperational property and related logic from AppError class

--- backend/src/utils/AppError.ts
+++ backend/src/utils/AppError.ts
@@ -1,14 +1,14 @@
 export class AppError extends Error {
   public readonly statusCode: number;
-  public readonly isOperational: boolean;
+  // public readonly isOperational: boolean;
 
-  constructor(statusCode: number, message: string, isOperational = true) {
+  constructor(statusCode: number, message: string) {
     super(message);
     this.statusCode = statusCode;
-    this.isOperational = isOperational;
+    // this.isOperational = isOperational;
 
     // Supaya instanceof bekerja dengan benar di TypeScript
-    Object.setPrototypeOf(this, new.target.prototype);
-    Error.captureStackTrace(this); // Ini berguna untuk Debugging. Baris ini memerintahkan program untuk mencatat di file mana dan baris ke berapa error ini terjadi,
+    // Object.setPrototypeOf(this, new.target.prototype);
+    // Error.captureStackTrace(this); // Ini berguna untuk Debugging. Baris ini memerintahkan program untuk mencatat di file mana dan baris ke berapa error ini terjadi,
   }
 }
